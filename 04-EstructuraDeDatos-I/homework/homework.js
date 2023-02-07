d793b08d4393397c5c0329bb9ac72186eae198a9
franko07 <franco11076@gmail.com>
2023-02-07 18:08:01 -0500

    Ejercicio terminado

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -3,20 +3,31 @@
 /*
 Definir las funciones recursivas nFactorial y nFibonacci.
 
-nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial (representado como n!) es el producto de n por todos los números naturales menores que él y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
+nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial (representado como n!) es el producto de n por todos los números naturales 
+menores que él y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
 
-nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
+nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que 
+cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
 Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
 
 Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
 
 
-Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
+Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera 
+iterativa.
 */
 
-function nFactorial(n) {}
+function nFactorial(n) {
+  if (n < 2 && n > -1) return 1       // devolvemos 1 puesto que 0! = 1 y 1! = 1
+  else if (n < 0) return 0        // Error no existe factorial de números negativos
+  return n * nFactorial(n - 1)
+}
 
-function nFibonacci(n) {}
+function nFibonacci(n) {
+  if (n <= 0) return 0;
+  else if (n === 1) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
+}
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
@@ -27,7 +38,32 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {}
+function Queue() {
+  this.data = [];
+}
+
+Queue.prototype.enqueue = function (value){
+  this.data.push(value)
+}
+
+Queue.prototype.dequeue = function() {
+  return this.data.shift();
+}
+
+Queue.prototype.size = function(){
+  return this.data.length;
+}
+
+let equality = new Queue();
+
+equality.enqueue()
+equality.enqueue()
+equality.dequeue()
+
+
+
+
+
 
 /*⚠️ No modificar nada debajo de esta línea ⚠️*/
 module.exports = {
