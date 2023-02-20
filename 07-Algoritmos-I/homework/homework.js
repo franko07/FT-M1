6fe348850a89747e0ff09fcae9f3802029b02dbd
franko07 <franco11076@gmail.com>
2023-02-20 18:47:11 -0500

    Ejercicio terminado

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let numPrimo = [1];
+  let divisor = 2;
+  while(num !== 1){
+    if (num % divisor === 0){
+      numPrimo.push(divisor);
+      num = num / divisor;
+    } else {
+      divisor++
+    }
+  }
+  return numPrimo;
 }
 
 function bubbleSort(array) {
@@ -14,15 +24,34 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+for (var i = 0; i <array.length; i++){
+  for(var j = 0 ; j < (array.length - i -1); j++){
+      if (array[j] > array[j+1]){
+          let temp = array[j];
+          array[j] = array[j+1];
+          array[j+1] = temp;
+      }
+  }
 }
-
+return array;
+}
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  let n = array.length;
+        for (let i = 1; i < n; i++) {
+            let current = array[i];
+            let j = i-1; 
+            while ((j > -1) && (current < array[j])) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = current;
+        }
+        return array;
 
 }
 
@@ -32,6 +61,22 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  let n = array.length;
+        
+    for(let i = 0; i < n; i++) {
+        let min = i;
+        for(let j = i+1; j < n; j++){
+            if(array[j] < array[min]) {
+                min=j; 
+            }
+         }
+         if (min != i) {
+             let tmp = array[i]; 
+             array[i] = array[min];
+             array[min] = tmp;      
+        }
+    }
+    return array;
 
 }
 
